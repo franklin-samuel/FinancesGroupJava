11e379121727fe11cb59bd9b4cae476c2997b30d
Samuel Franklin <samuelfranklinff@gmail.com>
2025-08-13 17:50:18 -0300

    Primeiro commit

---
+++ src/main/java/com/metas/meta_financeira/MetaFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package com.metas.meta_financeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaFinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaFinanceiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metas/meta_financeira/MetaFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metas.meta_financeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaFinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
