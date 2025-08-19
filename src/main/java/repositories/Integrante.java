d970299f22ffdd8460071995d587a698ca65b9c5
Samuel Franklin <samuelfranklinff@gmail.com>
2025-08-19 13:25:55 -0300

    feat: Repository
    Adicionei as interfaces Integrante, Meta e User para acesso ao banco de dados.

---
+++ src/main/java/repositories/Integrante.java
@@ -0,0 +1,5 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Integrante extends JpaRepository<Integrante, Long> {}
---
+++ src/main/java/repositories/UserRespository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import java.util.Optional;
+import com.metas.meta_financeira.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRespository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByOauthId(String oauthId);
+}
